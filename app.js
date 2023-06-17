5d2646021be281d623cb3c46e0840841a126e65f
Kashaf-Ansari-3108 <kashafansari3108@gmail.com>
2023-06-17 16:34:43 +0500

    done

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('dotenv').config({path:'./config.env'});
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/sample", (req,res)=>{
+app.get("/", (req,res)=>{
     res.send("API HIT!!!");
 });
 
